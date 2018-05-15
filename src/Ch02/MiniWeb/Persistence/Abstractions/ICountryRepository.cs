c4fbb2fc600bc53f9a0af8f67863944520902156
codeyu <codeyu@gmail.com>
2018-05-15 12:38:35 +0800

    Ch02-3

---
+++ src/Ch02/MiniWeb/Persistence/Abstractions/ICountryRepository.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+using MiniWeb.Persistence.Model;
+
+namespace MiniWeb.Persistence.Abstractions
+{
+    public interface ICountryRepository
+    {
+        IQueryable<Country> All();
+        Country Find(string code);
+        IQueryable<Country> AllBy(string filter);
+    }
+}
--- src/Ch02/MiniWeb/Startup.cs
+++ src/Ch02/MiniWeb/Startup.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using MiniWeb.Persistence;
+using MiniWeb.Persistence.Abstractions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace MiniWeb
 {
@@ -15,20 +16,43 @@ namespace MiniWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ICountryRepository>(new CountryRepository());
+            // Or, in alternative, let the system create one
+            //services.AddSingleton<ICountryRepository, CountryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,IServiceProvider provider)
         {
-            if (env.IsDevelopment())
+            app.Map("/country", countryApp =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                countryApp.Run(async (context) =>
+                {
+                    var country = provider.GetService<ICountryRepository>();
+                    var query = context.Request.Query["q"];
+                    var list = country.AllBy(query).ToList();
+                    var json = JsonConvert.SerializeObject(list);
 
+                    await context.Response.WriteAsync(json);
+                });
+            });
+
+            // Work as a catch-all
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Invalid call");
             });
+
+            // Try this code to see that without app.Map the same output is served for each URL
+            //app.Run(async (context) =>
+            //{
+            //    var country = provider.GetService<ICountryRepository>();
+            //    var query = context.Request.Query["q"];
+            //    var list = country.AllBy(query).ToList();
+            //    var json = JsonConvert.SerializeObject(list);
+
+            //    await context.Response.WriteAsync(json);
+            //});
         }
     }
 }
